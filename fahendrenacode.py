8b2ce2723b8b48558bdb4520f3b6faaa82f8f115
RakFahFita <skajirakotoarison@gmail.com>
2024-07-11 15:09:21 +0300

    suppression

--- fahendrenacode.py
+++
@@ -1,7 +0,0 @@
-print("Bonjour !")
-name = str(input("Votre nom s'il vous plait : "))
-age = int(input("Pouvez-vous entrer votre âge içi : "))
-if (age < 18):
-    print("Donc {}, vous êtes mineur, car votre âge est {} < 18.".format(name, age))
-else:
-    print("Donc {}, vous êtes mineur, car votre âge est {} > 18.".format(name, age))
